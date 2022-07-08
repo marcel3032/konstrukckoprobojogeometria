ab587c84116f65579b401eeb5e5aede0f851b686
Marcel Palaj <palaj.marcel@gmail.com>
2022-07-08 20:41:10 +0200

    init commit

---
+++ websocket-server/index.ino
@@ -0,0 +1,112 @@
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <style>
+  .card {
+     border-style: solid;
+  }
+  </style>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="content" id="content"></div>
+  <div>
+	  <table id="debug_log" border>
+		<tr><th>Time</th><th>Message</th></tr>
+	  </table>
+  </div>
+<script>
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', (e) => {
+    initWebSocket();
+  });
+  
+  function initWebSocket() {
+    console.log('Trying to open a WebSocket connection...');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = (ev => { console.log('Connection opened'); });
+    websocket.onclose = (ev => {
+	  console.log('Connection closed');
+      setTimeout(initWebSocket, 2000);
+	});
+    websocket.onmessage = onMessage;
+  }
+  
+  function onMessage(event) {
+    console.log(event.data);
+    let recieved_data = JSON.parse(event.data);
+    if(recieved_data.type=="message"){
+	  buttons.forEach( b => b.update(recieved_data));
+    }
+	if(recieved_data.type=="debug"){
+      document.getElementById('debug_log').innerHTML += "<tr><td>"+recieved_data.date+"</td><td>"+recieved_data.data+"</td></tr>";
+    }
+  }
+  
+  const $e = (elName, cl, parEl) => {
+    let e = document.createElement(elName);
+    cl.forEach( val => e.setAttribute(val[0], val[1]));
+
+    if (parEl) parEl.appendChild (e);
+    return e;
+  }
+  
+  class PortOutput{
+	constructor(port) {
+	  this.port = port;
+	  this.value = 0;
+	  this.el = $e("div", [["class", "card"]], document.getElementById("content"));
+	  this.el.innerHTML = "Port: "+port;
+	  this.description = $e("div", [], this.el);
+	  this.description.innerHTML = "current state: "+this.value;
+	}
+  }
+  
+  class Button extends PortOutput{
+	constructor(port) {
+	  super(port);
+	  this.button = $e("button", [], this.el);
+	  this.button.innerHTML = "port: "+port;
+	  this.button.addEventListener('click', ev => {
+	    let send_value = 1 - this.value;
+	    websocket.send('{"port":'+this.port+', "analog":false, "value":'+send_value+'}');
+	  });
+	}
+	
+    update(recieved_data){
+		if(recieved_data.port==this.port){
+			this.description.innerHTML = "current state: "+recieved_data.data;
+			this.value = recieved_data.data;
+		}
+	}
+  }
+	
+  class Slider extends PortOutput {
+	constructor(port) {
+	  super(port);
+	  this.button = $e("input", [["type", "range"], ["min", "0"], ["max", "255"]], this.el);
+	  this.button.value = 0;
+	  this.button.addEventListener('input', ev => {
+	    let send_value = this.button.value;
+	    websocket.send('{"port":'+this.port+', "analog":true, "value":'+send_value+'}');
+	  });
+	}
+	
+    update(recieved_data){
+		if(recieved_data.port==this.port){
+			this.description.innerHTML = "current state: "+recieved_data.data;
+			this.value = recieved_data.data;
+		}
+	}
+  }
+  
+  var buttons = [];
+  buttons.push(new Button(22));
+  buttons.push(new Slider(22));
+</script>
+</body>
+</html>
+)rawliteral";
---
+++ websocket-server/websocket-server.ino
@@ -0,0 +1,89 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+
+// Replace with your network credentials
+const char* ssid = "TODO";
+const char* password = "TODO";
+
+const int ledPin = 22;
+extern const char index_html[];
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void notifyClients(long port, long value) {
+  ws.textAll("{ \"type\": \"message\", \"data\": "+String(value)+", \"port\": 22}");
+  ws.textAll("{ \"type\": \"debug\", \"date\": "+String(millis())+", \"data\": \"prepol som ledku "+String(port)+": "+String(value)+"\"}");
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, data);
+    long port = doc["port"];
+    bool analog = doc["analog"];
+    long value = doc["value"];
+    if(analog)
+      analogWrite(port, value);
+    else
+      digitalWrite(port, value);
+    
+    notifyClients(port, value);
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup(){
+  Serial.begin(115200);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  server.begin();
+}
+
+void loop() {
+  ws.cleanupClients();
+}
